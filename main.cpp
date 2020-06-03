ff259b5bf3c2031181c091e2751ba64266edb397
WeiChinHsu-op <eric87092611@gmail.com>
2020-06-03 16:16:09 +0800

    333

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "mbed.h"
 #include "mbed_rpc.h"
+#include "fsl_port.h"
+#include "fsl_gpio.h"
+#define UINT14_MAX        16383
+ // FXOS8700CQ I2C address
+#define FXOS8700CQ_SLAVE_ADDR0 (0x1E<<1) // with pins SA0=0, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR1 (0x1D<<1) // with pins SA0=1, SA1=0
+#define FXOS8700CQ_SLAVE_ADDR2 (0x1C<<1) // with pins SA0=0, SA1=1
+#define FXOS8700CQ_SLAVE_ADDR3 (0x1F<<1) // with pins SA0=1, SA1=1
+ // FXOS8700CQ internal register addresses
+#define FXOS8700Q_STATUS 0x00
+#define FXOS8700Q_OUT_X_MSB 0x01
+#define FXOS8700Q_OUT_Y_MSB 0x03
+#define FXOS8700Q_OUT_Z_MSB 0x05
+#define FXOS8700Q_M_OUT_X_MSB 0x33
+#define FXOS8700Q_M_OUT_Y_MSB 0x35
+#define FXOS8700Q_M_OUT_Z_MSB 0x37
+#define FXOS8700Q_WHOAMI 0x0D
+#define FXOS8700Q_XYZ_DATA_CFG 0x0E
+#define FXOS8700Q_CTRL_REG1 0x2A
+#define FXOS8700Q_M_CTRL_REG1 0x5B
+#define FXOS8700Q_M_CTRL_REG2 0x5C
+#define FXOS8700Q_WHOAMI_VAL 0xC7
+float t[3];
+float v[2];
 
-RawSerial pc(USBTX, USBRX);
-RawSerial xbee(D12, D11);
+void getvelocity(Arguments *in, Reply *out);
 
-EventQueue queue(32 * EVENTS_EVENT_SIZE);
-Thread t;
+RPCFunction rpcnumber(&getvelocity, "getvelocity");
 
 void pc_rx_interrupt(void);
 void pc_rx(void);
@@ -14,11 +36,78 @@ void xbee_rx(void);
 void reply_messange(char *xbee_reply, char *messange);
 void check_addr(char *xbee_reply, char *messenger);
 
+I2C i2c( PTD9,PTD8);
+RawSerial pc(USBTX, USBRX);
+RawSerial xbee(D12, D11);
+int m_addr = FXOS8700CQ_SLAVE_ADDR1;
 
-int main()
-{
-  pc.baud(9600);
-  char xbee_reply[4];
+InterruptIn sw2(SW2);
+ 
+EventQueue queue1(32 * EVENTS_EVENT_SIZE);
+Thread thread1;
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
+Thread thread2;
+
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len);
+void FXOS8700CQ_writeRegs(uint8_t * data, int len);
+  
+void acc_call()  {
+    pc.baud(9600);
+    
+    uint8_t who_am_i, data[2], res[6];
+    int16_t acc16;
+    
+    int a;
+    //int count;
+   // Enable the FXOS8700Q
+
+    FXOS8700CQ_readRegs( FXOS8700Q_CTRL_REG1, &data[1], 1);
+    data[1] |= 0x01;
+    data[0] = FXOS8700Q_CTRL_REG1;
+    FXOS8700CQ_writeRegs(data, 2);
+
+    // Get the slave address
+    FXOS8700CQ_readRegs(FXOS8700Q_WHOAMI, &who_am_i, 1);
+ 
+    //uLCD.printf("Here is %x\r\n", who_am_i);
+    //while (true) {
+    
+    while(true){
+        
+        FXOS8700CQ_readRegs(FXOS8700Q_OUT_X_MSB, res, 6);
+ 
+        acc16 = (res[0] << 6) | (res[1] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        t[0] = ((float)acc16) / 4096.0f;
+ 
+        acc16 = (res[2] << 6) | (res[3] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        t[1] = ((float)acc16) / 4096.0f;
+
+        acc16 = (res[4] << 6) | (res[5] >> 2);
+        if (acc16 > UINT14_MAX/2)
+            acc16 -= UINT14_MAX;
+        t[2] = ((float)acc16) / 4096.0f;
+
+        wait(0.1);
+    }
+}
+
+void acc_thread(){
+    
+    queue1.call(acc_call);
+   
+}
+
+int main() {
+    thread1.start(callback(&queue1, &EventQueue::dispatch_forever));
+    
+    sw2.rise(acc_thread);
+    
+    char xbee_reply[4];
 
   // XBee setting
   xbee.baud(9600);
@@ -30,7 +119,6 @@ int main()
     xbee_reply[0] = '\0';
     xbee_reply[1] = '\0';
   }
-
   xbee.printf("ATMY 0x240\r\n");
   reply_messange(xbee_reply, "setting MY : 0x240");
 
@@ -55,12 +143,81 @@ int main()
 
   // start
   pc.printf("start\r\n");
-  t.start(callback(&queue, &EventQueue::dispatch_forever));
+    thread2.start(callback(&queue, &EventQueue::dispatch_forever));
 
-  // Setup a serial interrupt function of receiving data from pc
-  pc.attach(pc_rx_interrupt, Serial::RxIrq);
-  // Setup a serial interrupt function of receiving data from xbee
-  xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
+    pc.attach(pc_rx_interrupt, Serial::RxIrq);
+    
+    xbee.attach(xbee_rx_interrupt, Serial::RxIrq);
+}
+
+void xbee_rx_interrupt(void)
+{
+  xbee.attach(NULL, Serial::RxIrq); // detach interrupt
+  queue.call(&xbee_rx);
+}
+
+void xbee_rx(void)
+{
+  char buf[100] = {0};
+  char outbuf[100] = {0};
+  while(xbee.readable()){
+    for (int i=0; ; i++) {
+      char recv = xbee.getc();
+      if (recv == '\r') {
+        pc.printf("\r\n");
+        break;
+      }
+      buf[i] = pc.putc(recv);
+    }
+    RPC::call(buf, outbuf);
+    pc.printf("%s\r\n", outbuf);
+    wait(0.1);
+  }
+  xbee.attach(xbee_rx_interrupt, Serial::RxIrq); // reattach interrupt
+}
+
+void reply_messange(char *xbee_reply, char *messange){
+  xbee_reply[0] = xbee.getc();
+  xbee_reply[1] = xbee.getc();
+  xbee_reply[2] = xbee.getc();
+  if(xbee_reply[1] == 'O' && xbee_reply[2] == 'K'){
+    pc.printf("%s\r\n", messange);
+    xbee_reply[0] = '\0';
+    xbee_reply[1] = '\0';
+    xbee_reply[2] = '\0';
+  }
+}
+
+void check_addr(char *xbee_reply, char *messenger){
+  xbee_reply[0] = xbee.getc();
+  xbee_reply[1] = xbee.getc();
+  xbee_reply[2] = xbee.getc();
+  xbee_reply[3] = xbee.getc();
+  pc.printf("%s = %c%c%c\r\n", messenger, xbee_reply[1], xbee_reply[2], xbee_reply[3]);
+  xbee_reply[0] = '\0';
+  xbee_reply[1] = '\0';
+  xbee_reply[2] = '\0';
+  xbee_reply[3] = '\0';
+}
+
+void getvelocity(Arguments *in, Reply *out){
+    // v = v0 + a*t
+    v[0] = v[0] + t[0]*0.1 //x direction
+    v[1] = v[1] + t[1]*0.1 //y direction
+     //call rpcfuction every 0.1s in python
+   
+    xbee.printf("%1.4f   %1.4f   \r\n",v[0],v[1]);
+    
+}
+
+void FXOS8700CQ_readRegs(int addr, uint8_t * data, int len) {
+    char t = addr;
+    i2c.write(m_addr, &t, 1, true);
+    i2c.read(m_addr, (char *)data, len);
+}
+
+void FXOS8700CQ_writeRegs(uint8_t * data, int len) {
+    i2c.write(m_addr, (char *)data, len);
 }
 
 void pc_rx_interrupt(void)
@@ -93,70 +250,3 @@ void pc_rx(void)
   wait(0.001);
   pc.attach(pc_rx_interrupt, Serial::RxIrq); // reattach interrupt
 }
-
-void xbee_rx_interrupt(void)
-{
-  xbee.attach(NULL, Serial::RxIrq); // detach interrupt
-  queue.call(&xbee_rx);
-}
-
-void xbee_rx(void)
-{
-  while(xbee.readable()){
-    char c = xbee.getc();
-    if(c!='\r' && c!='\n'){
-      pc.printf("%c",c);
-    }else{
-      pc.printf("\r\n");
-    }
-  }
-  wait(0.001);
-  xbee.attach(xbee_rx_interrupt, Serial::RxIrq); // reattach interrupt
-}
-
-void reply_messange(char *xbee_reply, char *messange){
-  xbee_reply[0] = xbee.getc();
-  xbee_reply[1] = xbee.getc();
-  xbee_reply[2] = xbee.getc();
-  if(xbee_reply[1] == 'O' && xbee_reply[2] == 'K'){
-    pc.printf("%s\r\n", messange);
-    xbee_reply[0] = '\0';
-    xbee_reply[1] = '\0';
-    xbee_reply[2] = '\0';
-  }
-}
-
-void check_addr(char *xbee_reply, char *messenger){
-  xbee_reply[0] = xbee.getc();
-  xbee_reply[1] = xbee.getc();
-  xbee_reply[2] = xbee.getc();
-  xbee_reply[3] = xbee.getc();
-  pc.printf("%s = %c%c%c\r\n", messenger, xbee_reply[1], xbee_reply[2], xbee_reply[3]);
-  xbee_reply[0] = '\0';
-  xbee_reply[1] = '\0';
-  xbee_reply[2] = '\0';
-  xbee_reply[3] = '\0';
-}
-
-void getnumber(Arguments *in, Reply *out){
-  int ans;
-  //int count;
-  //int get;
-  int count_previos;
-   if(initial==0){
-     xbee.printf("%d",number);
-     pc.printf("%d",number);
-     initial++;
-   }
-   else{
-     ans = number-count_previous;
-     xbee.printf("%d",ans);
-     pc.printf("%d",ans);
-   }
-
-   count_previous = number;
-}
-
-
-
-
